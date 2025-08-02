2ba25758569d52295ee07e1724f3183b8301c434
Tofik Khan <tofik.khan@mkausa.org>
2025-08-01 20:58:44 -0700

    Add initial theme provider and basic colors

---
+++ src/lib/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: {
+    light: "#212D97",
+    main: "#18216d",
+    dark: "#121954",
+    contrastText: "#fff",
+  },
+  secondary: {
+    light: "#4f78c1",
+    main: "#2457B2",
+    dark: "#193c7c",
+    contrastText: "#fff",
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { BrowserRouter } from "react-router";
+import { theme } from "@/theme/mainTheme";
+import { ThemeProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
